94aea2f386d97829d5db6880efcc113f546ea872
uchi0115 <gu.car.c115@gmail.com>
2020-12-21 21:11:43 +0900

    Initialize repository

---
+++ app/firebase/data_set.js
@@ -0,0 +1,27 @@
+var admin = require("firebase-admin");
+
+// 1. サービスアカウント鍵を生成しserviceAccountKey.jsonでリネームしてフォルダ直下に配置
+var serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  // 2. Realtime DatabaseのページでdatabaseURLを確認して反映
+  databaseURL: "https://tic-matching-default-rtdb.firebaseio.com/"
+});
+
+var db = admin.database();
+var ref = db.ref("server/saving-data");
+
+var usersRef = ref.child("users");
+usersRef.set({
+    "userID": "chibaren",
+    "password": "ticmatching"
+});
+
+ref.on("value", function(snapshot) {
+    console.log("value Changed!!!");
+    console.log(snapshot.val());
+}, 
+function(errorObject) {
+    console.log("failed: " + errorObject.code);
+} );
