4bc72b5f69cdfb3506c66c8d793b9e7127c15340
uchi0115 <gu.car.c115@gmail.com>
2021-02-22 11:31:57 +0900

    add function makeChatroom

---
+++ app/routes/Chat.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+var push = require('../firebase/data_push.js');
+var set = require('../firebase/data_set.js');
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+module.exports = router;
+
+/*
+chatroom {
+    roomA {
+        from : "Taro"
+        to : "Ken"
+        messages {
+            message1 {            
+                from : "Taro"
+                message : "Hello"
+*/
+
+function makeChatroom(){
+    router.post('/', function(req, res, next) {
+        const chat = {
+        userid: req.body.userid,
+        userid2: req.body.userid2
+    
+        }
+    
+        // ここにFirebaseへのデータ追加関数呼び出しを書く
+        push.data_push("chatroom",chat);
+        res.status(200).send(chat);
+    })
+}
+
