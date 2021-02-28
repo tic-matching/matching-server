ceea8df48512a4620d056f9b6ada647cd8a65d9c
S-Kaito <irc.darjeeling@gmail.com>
2021-02-28 19:21:30 +0900

    Apply client

--- app/firebase/data_set.js
+++ app/firebase/data_set.js
@@ -3,11 +3,13 @@ var admin = require("firebase-admin");
 // 1. サービスアカウント鍵を生成しserviceAccountKey.jsonでリネームしてフォルダ直下に配置
 var serviceAccount = require("./serviceAccountKey.json");
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  // 2. Realtime DatabaseのページでdatabaseURLを確認して反映
-  databaseURL: "https://tic-matching-default-rtdb.firebaseio.com/"
-});
+if (admin.apps.length === 0) { //2重起動防止
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    // 2. Realtime DatabaseのページでdatabaseURLを確認して反映
+    databaseURL: "https://tic-matching-default-rtdb.firebaseio.com/"
+  });
+}
 
 var db = admin.database();
 var ref = db.ref("server/saving-data");
